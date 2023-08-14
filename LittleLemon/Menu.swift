405a82c942efd220ea8864e273672ea8b2497b46
Carlos Martínez <carlos@tr3sco.com>
2023-08-13 22:42:55 -0600

    Show menu data from remote url.

--- LittleLemon/Menu.swift
+++ LittleLemon/Menu.swift
@@ -8,15 +8,71 @@
 import SwiftUI
 
 struct Menu: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var searchText = ""
+    
     var body: some View {
         VStack {
             Text("Little Lemon")
             Text("Chicago")
             Text("Little Lemon is a local mediterranean restaurant")
-            List {
-                
+            TextField("Search menu", text: $searchText)
+                .padding(.all, 20)
+            FetchedObjects(predicate: buildPredicate(), sortDescriptors: buildSortDescriptors()) { (dishes: [Dish]) in
+                List {
+                    ForEach(dishes) { dish in
+                        HStack {
+                            Text("\((dish.title ?? "")) \(dish.price ?? "")")
+                            AsyncImage(url: URL(string: dish.image ?? "")) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 70, height: 70)
+                        }
+                    }
+                }
             }
         }
+        .onAppear {
+            getMenuData()
+        }
+    }
+    
+    func getMenuData() {
+        PersistenceController.shared.clear()
+        let serverAddress = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        let url = URL(string: serverAddress)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                let recipes = try? decoder.decode(MenuList.self, from: data)
+                recipes?.menu.forEach {
+                    let dish = Dish(context: viewContext)
+                    dish.title = $0.title
+                    dish.image = $0.image
+                    dish.price = $0.price
+                    dish.category = $0.category
+                }
+                try? viewContext.save()
+            }
+        }
+        task.resume()
+    }
+    
+    func buildSortDescriptors() -> [NSSortDescriptor] {
+        return [
+            NSSortDescriptor(key: "title", ascending: true, selector: #selector(NSString.localizedStandardCompare))
+        ]
+    }
+    
+    func buildPredicate() -> NSPredicate {
+        let cleanText = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        return cleanText.isEmpty ? NSPredicate(value: true) : NSPredicate(format: "title CONTAINS[cd] %@", cleanText)
     }
 }
 
